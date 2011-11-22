d42bfce01c151756c9e88ca7adf18046a26e4aa4
Rafidzal Rafiq <rafidzal.rafiq@systemic.com.au>
2011-11-22 21:48:22 +0800

    Added SifWork ADK.

---
+++ Tools/SifWorks-Adk.Net-2.3.2.1-AU-release/examples/CSharp/SimpleProvider/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly : AssemblyTitle( "SimpleProvider" )]
+[assembly : AssemblyDescription( "" )]
+[assembly : AssemblyConfiguration( "" )]
+[assembly : AssemblyCompany( "Edustructures LLC" )]
+[assembly : AssemblyProduct( "SimpleProvider" )]
+[assembly : AssemblyCopyright( "Copyright © Edustructures LLC 2006" )]
+[assembly : AssemblyTrademark( "" )]
+[assembly : AssemblyCulture( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly : ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly : Guid( "7fd13e3e-771f-4fe1-99fd-ef7674b2b77b" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly : AssemblyVersion( "1.0.0.0" )]
+[assembly : AssemblyFileVersion( "1.0.0.0" )]
